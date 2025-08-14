820ee9585f66d3a16b2f3b4315fa23e5f75216cd
Ardivan Nur Raihan Rahman <ardivannurraihanr@gmail.com>
2025-08-14 11:19:31 +0700

    update repo

--- AplikasiWarga/AplikasiWarga.GUI/obj/Debug/net9.0/AplikasiWarga.GUI.AssemblyInfo.cs
+++ AplikasiWarga/AplikasiWarga.GUI/obj/Debug/net9.0/AplikasiWarga.GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplikasiWarga.GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d32e447fd7e9729556f7ebe43f4ad9fabb2b8cbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e3c4cad41c5876f4dcf7a02fc41ae8ea7cbc58")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplikasiWarga.GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplikasiWarga.GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
